f25f075aa0d532ded1ec70b8dad157b1fcda592b
kanhaiya41 <161762630+kanhaiya41@users.noreply.github.com>
2026-04-04 11:28:11 +0530

    url changes

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "https://hire-hub-psi.vercel.app/api/v1";
-const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = "https://job-portal-backend-2srt.onrender.com/api/v1";
 export const USER_API_END_POINT = `${BASE_URL}/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/job`;
 export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
